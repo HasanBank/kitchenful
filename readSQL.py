cc18370adea9fba128154418adfc7a2cb9476704
hasan1234-7 <has_bak@hotmail.com>
2020-03-11 16:56:05 +0100

    Inner Join has been implemented

--- readSQL.py
+++ readSQL.py
@@ -2,30 +2,49 @@ import sqlite3
 import connectSQL
 
 def readAllProducts(sqliteConnection,cursor):
-
 	sqlite_select_query = """SELECT * from products"""
-	printResult(cursor,sqlite_select_query)
+	printOrExecuteSQL(cursor,sqlite_select_query,True)
 
 def getAmount(sqliteConnection,cursor,productName):
 	keyword = "%" + productName + "%"
-	sqlite_select_query = " SELECT Amount, Amount_Unit from products where Name LIKE '%s'" %keyword
-	printResult(cursor,sqlite_select_query)
+	sqlite_select_query = " SELECT name,amount, amount_Unit from products where Name LIKE '%s'" %keyword
+	printOrExecuteSQL(cursor,sqlite_select_query,True)
+
+def getAmountOfIngredientsOfRecipe(sqliteConnection,cursor,recipeName):
+	keyword = "%" + recipeName + "%"
+	amountOfIngredients = "Amount of Ingredients"
+
+	sqlite_select_query = " SELECT id from recipes where Name LIKE '%s'" %keyword
+	sqlite_merged_query = " Select Count(recipes_ingredients.ingredient_id) as '%s' , recipes.name FROM recipes_ingredients INNER JOIN recipes ON recipes_ingredients.recipe_id = recipes.id Where recipes.id In (%s) GROUP BY recipes_ingredients.recipe_id " %(amountOfIngredients, sqlite_select_query)  
+	print(sqlite_merged_query)
+
+	printOrExecuteSQL(cursor,sqlite_merged_query,True)
 
 
-def printResult(cursor,sqlite_select_query):
+def printOrExecuteSQL(cursor,sqlite_select_query,printResult):
 	cursor.execute(sqlite_select_query)
 	result = cursor.fetchall()
-	print("Result:  ", result)
+	if(printResult):
+		print('### Result ###')
+		print( [i[0] for i in cursor.description])
+		print('\n'.join(str(e) for e in result))
+	else:
+		return result
 	cursor.close()
+
+
+
+
 
 
 def main():
 	print('# Press enter the key #')
 	print('To read all prodcuts: 1')
 	print('To get amount of the product: 2')
+	print('To get number of ingredients in the recipe: 3')
 	key = int(input())
 
-	if key != 1 and key != 2:
+	if key != 1 and key != 2 and key != 3:
 		print('Please enter an acceptable key ')
 		main()
 	else:
@@ -36,6 +55,9 @@ def main():
 		if key == 2:
 			productName = input('Please enter the product name: ')
 			getAmount(sqliteConnection,cursor,productName)
+		if key == 3:
+			recipeName = input('Please enter the recipe name: ')
+			getAmountOfIngredientsOfRecipe(sqliteConnection,cursor,recipeName)
 
 		connectSQL.closeConnection(sqliteConnection)
 
